7451ea5684b766220a8a3475082434408c240548
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-05 08:03:08 +0700

    :newspaper: create screen list

---
+++ My UI Kit Playground/AppListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppListViewController.swift
+//  My UI Kit Playground
+//
+//  Created by Yusuf Saifudin on 05/04/23.
+//
+
+import UIKit
+
+class AppListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
