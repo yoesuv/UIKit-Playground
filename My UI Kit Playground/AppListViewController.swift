54a981297515604925b660e3774d706eff9bb17e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-11 15:51:11 +0700

    :sparkles: show data table view

--- My UI Kit Playground/AppListViewController.swift
+++ My UI Kit Playground/AppListViewController.swift
@@ -7,18 +7,36 @@
 
 import UIKit
 
-class AppListViewController: UIViewController {
+class AppListViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var listData: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for number in 1...100 {
+            listData.append("Data ke \(number)")
+        }
         
         let backButton = UIBarButtonItem()
         backButton.title = "Back"
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
         
         self.navigationItem.title = "List View"
+        
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath) as! DataTableViewCell
+        cell.label.text = listData[indexPath.row]
+        return cell
     }
 
 }
