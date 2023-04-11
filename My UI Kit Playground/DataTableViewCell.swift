e2b3b4e580de218e1eec442a5f289c0aec982404
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-11 14:41:02 +0700

    :pencil: setup cell

---
+++ My UI Kit Playground/DataTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DataTableViewCell.swift
+//  My UI Kit Playground
+//
+//  Created by Yusuf Saifudin on 11/04/23.
+//
+
+import UIKit
+
+class DataTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+
+}
