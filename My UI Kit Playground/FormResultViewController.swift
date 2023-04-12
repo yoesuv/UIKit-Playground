759502beb8f7ba5e4ff77675cbf1068406fa0281
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-12 08:55:27 +0700

    :newspaper: create form result view controller

---
+++ My UI Kit Playground/FormResultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FormResultViewController.swift
+//  My UI Kit Playground
+//
+//  Created by Yusuf Saifudin on 12/04/23.
+//
+
+import UIKit
+
+class FormResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
