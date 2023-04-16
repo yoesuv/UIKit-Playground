210d3130801f5a63c1c6dfb57d8f199468e001de
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-16 10:42:04 +0700

    :newspaper: UI dialog logout

---
+++ My UI Kit Playground/Alert/DialogLogoutViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DialogLogoutViewController.swift
+//  My UI Kit Playground
+//
+//  Created by Yusuf Saifudin on 16/04/23.
+//
+
+import UIKit
+
+class DialogLogoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
