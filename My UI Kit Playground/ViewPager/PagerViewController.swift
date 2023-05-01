cedba02773f8e20b672a8be4690e70c877a112b6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-05-01 10:27:01 +0700

    :construction: setup tabview content

--- My UI Kit Playground/ViewPager/PagerViewController.swift
+++ My UI Kit Playground/ViewPager/PagerViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PagerViewController: UIViewController {
 
+    @IBOutlet weak var tabsView: TabsView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +19,25 @@ class PagerViewController: UIViewController {
         self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
         
         self.navigationItem.title = "View Pager"
+        
+        setupTabs()
+    }
+    
+    private func setupTabs() {
+        tabsView.tabs = [
+            Tab(icon: UIImage(systemName: "music.note"), title: "Music"),
+            Tab(icon: UIImage(systemName: "video.square"), title: "Movies"),
+            Tab(icon: UIImage(systemName: "text.book.closed"), title: "Books")
+        ]
+        
+        tabsView.tabMode = .fixed
+        
+        tabsView.titleColor = .black
+        tabsView.iconColor = .black
+        tabsView.backgroundColor = UIColor(named: "Grey200")
+        tabsView.titleFont = UIFont.systemFont(ofSize: 15, weight: .regular)
+        
+        tabsView.collectionView.selectItem(at: IndexPath(item: 0, section: 0), animated: true, scrollPosition: .centeredVertically)
     }
 
 }
