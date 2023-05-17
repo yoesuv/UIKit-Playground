7e8aae7d854615d834a779223c156cf5949129d6
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-05-17 16:36:35 +0700

    :pencil: hide navigation bar

--- My UI Kit Playground/Collapsing/CollapsingViewController.swift
+++ My UI Kit Playground/Collapsing/CollapsingViewController.swift
@@ -12,5 +12,15 @@ class CollapsingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 
 }
